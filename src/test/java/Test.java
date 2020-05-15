cd117a37d20f6bc381d7229e7ba9eea0bdd28f90
Santosh Marigowda <santosharakere@gmail.com>
2020-05-15 08:17:35 +0100

    capture the response in a String variable and print it

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,16 +7,18 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello and Welcome to REST API Testing using Rest Assured...");
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all()
+        String response = given().log().all()
                 .queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .body(PayLoad.AddPlace())
                 .when()
                     .post("/maps/api/place/add/json")
-                .then().log().all()
+                .then()
                     .assertThat()
                         .statusCode(200)
                         .body("scope", equalTo("APP"))
-                        .header("server", equalTo("Apache/2.4.18 (Ubuntu)"));
+                        .header("server", equalTo("Apache/2.4.18 (Ubuntu)"))
+                    .extract().response().asString();
+        System.out.println("Response Extracted is:\n" + response);
     }
 }
