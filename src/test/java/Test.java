38fd850c1617c6ca27872e95eb77e3bebd029b21
Santosh Marigowda <santosharakere@gmail.com>
2020-05-15 10:23:37 +0100

    get the updated place and verify the address

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -30,16 +30,25 @@ public class Test {
         System.out.println(placeId);
 
         // Update the address of place added in previous step
+        String newAddress = "70 Summer walk, USA";
         given().log().all()
                 .header("Content-Type", "application/json")
                 .body("{\n" +
                         "\"place_id\":\"" + placeId + "\",\n" +
-                        "\"address\":\"70 Summer walk, USA\",\n" +
+                        "\"address\":\"" + newAddress + "\",\n" +
                         "\"key\":\"qaclick123\"\n" +
                         "}")
                 .when()
                     .put("/maps/api/place/update/json")
                 .then().log().all()
                     .assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        // Get the updated place and verify that the address is updated
+        given().log().all()
+                .queryParam("key", "qaclick123").queryParam("place_id", placeId)
+                .when()
+                    .get("/maps/api/place/get/json")
+                .then().log().all()
+                    .assertThat().statusCode(200).body("address", equalTo(newAddress));
     }
 }
