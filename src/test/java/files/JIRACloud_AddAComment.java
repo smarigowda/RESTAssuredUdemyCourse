92f0afc4fd1b12e8bd84512341b13e8531d5cd1d
Santosh Marigowda <santosharakere@gmail.com>
2020-05-17 19:38:51 +0100

    adding a comment to an issue

---
+++ src/test/java/files/JIRACloud_AddAComment.java
@@ -0,0 +1,28 @@
+package files;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import org.testng.annotations.Test;
+
+public class JIRACloud_AddAComment {
+    @Test
+    public void AddAComment() {
+        RestAssured.baseURI = "https://santosharakere.atlassian.net";
+
+        PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+        authScheme.setUserName("santosharakere@gmail.com");
+        authScheme.setPassword(System.getenv("JIRA_API_TOKEN"));
+        RestAssured.authentication = authScheme;
+
+        RestAssured
+                .given().log().all()
+                    .header("Content-Type", "application/json")
+                    .pathParam("issueId", "10000")
+                    .body("{\n" +
+                            "  \"body\": \"Comment 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper.\"\n" +
+                            "}")
+                .when().log().all()
+                    .post("/rest/api/2/issue/{issueId}/comment")
+                .then().log().all();
+    }
+}
