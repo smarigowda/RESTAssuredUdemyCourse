2aa47ac5f74a2952cb8e700f7fe7be853f133d9a
Santosh Marigowda <santosharakere@gmail.com>
2020-05-17 16:52:07 +0100

    create a new issue in JIRA Cloud

--- src/test/java/JIRACloudAPITest.java
+++ src/test/java/JIRACloudAPITest.java
@@ -7,34 +7,35 @@ import org.testng.annotations.Test;
 public class JIRACloudAPITest {
     @Test
     public void CreateIssue() {
-        RestAssured.baseURI = "http://santosharakere.atlassian.net";
+        RestAssured.baseURI = "https://santosharakere.atlassian.net";
 
-        System.out.println(System.getenv("JIRA_API_TOKEN"));
         PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
         authScheme.setUserName("santosharakere@gmail.com");
         authScheme.setPassword(System.getenv("JIRA_API_TOKEN"));
         RestAssured.authentication = authScheme;
 
-        String response1 = RestAssured
-                .given()
+        // Create an Issue
+        String response = RestAssured
+                .given().log().all()
+                    .header("Content-Type", "application/json")
+                    .body("{\n" +
+                            "\t\"fields\": {\n" +
+                            "\t\t\"project\": {\n" +
+                            "\t\t\t\"key\": \"RES\"\n" +
+                            "\t\t},\n" +
+                            "\t\t\"summary\": \"my Second Bug\",\n" +
+                            "\t\t\"description\": \"A Bug logged using REST API\",\n" +
+                            "\t\t\"issuetype\": {\n" +
+                            "\t\t\t\"name\": \"Bug\"\n" +
+                            "\t\t}\n" +
+                            "\t}\n" +
+                            "}")
                 .when()
-                    .get("/rest/api/2/issue/RES-1")
+                    .post("/rest/api/2/issue")
                 .then()
-                    .assertThat().statusCode(200)
+                    .log().all()
                     .extract().response().asString();
 
-        System.out.println("Response 1....");
-        System.out.println(response1);
-
-        String response2 = RestAssured
-                            .given()
-                            .when().log().all()
-                                .get("/rest/api/2/issue/RES-1")
-                            .then()
-                                .assertThat().statusCode(200)
-                                .extract().response().asString();
-        System.out.println("Response 2....");
-        System.out.println(response2);
-
+        System.out.println(response);
     }
 }
