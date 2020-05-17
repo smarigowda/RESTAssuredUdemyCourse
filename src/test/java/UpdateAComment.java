34630ab41d207978a11b517844db330f9456468f
Santosh Marigowda <santosharakere@gmail.com>
2020-05-17 21:05:08 +0100

    code to update a comment

--- src/test/java/UpdateAComment.java
+++ src/test/java/UpdateAComment.java
@@ -11,5 +11,18 @@ public class UpdateAComment {
         authScheme.setUserName("santosharakere@gmail.com");
         authScheme.setPassword(System.getenv("JIRA_API_TOKEN"));
         RestAssured.authentication = authScheme;
+
+        RestAssured
+                .given()
+                    .header("Content-Type", "application/json")
+                    .pathParam("issueId", "10000")
+                    .pathParam("commentId", "10003")
+                    .body("{\n" +
+                        "  \"body\": \"Comment updated using REST API, using RestAssured framework\"\n" +
+                        "}")
+                .when().put("/rest/api/2/issue/{issueId}/comment/{commentId}")
+                .then().log().all()
+                    .statusCode(200);
+
     }
 }
