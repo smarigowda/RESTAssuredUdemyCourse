d5303b87e63e63b68dd134c746efdaebe7dd0110
Santosh Marigowda <santosharakere@gmail.com>
2020-05-18 06:18:31 +0100

    iterate all comments and assert comment text

---
+++ src/test/java/jiracloud/GETIssueDetails.java
@@ -0,0 +1,47 @@
+package jiracloud;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GETIssueDetails {
+    @Test
+    public void getIssueDetails() {
+        RestAssured.baseURI = "https://santosharakere.atlassian.net";
+
+        PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+        authScheme.setUserName("santosharakere@gmail.com");
+        authScheme.setPassword(System.getenv("JIRA_API_TOKEN"));
+        RestAssured.authentication = authScheme;
+
+        String expAttachmentId = "10000";
+        String expCommentId = "10003";
+
+        String response = RestAssured
+                .given()
+                    .pathParam("issueId", "10000")
+                    .queryParam("fields", "comment")
+                .when()
+                    .get("/rest/api/3/issue/{issueId}")
+                .then()
+                    .assertThat().statusCode(200)
+                    .extract().response().asString();
+
+        System.out.println(response);
+
+        JsonPath js = new JsonPath(response);
+        int count = js.getInt("fields.comment.comments.size()");
+        System.out.println(count);
+        for(int i=0; i<count; i++) {
+            String commentId = js.getString(String.format("fields.comment.comments[%d].id", i));
+            System.out.println(commentId);
+            if(commentId.equals(expCommentId)) {
+                String commentText = js.getString(String.format("fields.comment.comments[%d].body.content[0].content[0].text", i));
+                System.out.println(commentText);
+                Assert.assertEquals(commentText, "Comment updated using REST API, using RestAssured framework");
+            }
+        }
+    }
+}
